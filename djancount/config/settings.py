0b2a663c577cecdcd80bc4462fdbf853e6fcf3bf
WilFriite <jnguessan2003@gmail.com>
2026-08-27 12:46:11 +0200

    Configuration Rest Framework pour l'authentification

--- djancount/config/settings.py
+++ djancount/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expenses',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES" : [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticatedOrReadOnly",],
+}
+
 
 
 # Database
